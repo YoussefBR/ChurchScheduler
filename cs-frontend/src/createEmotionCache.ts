e39446fb61ad15564fc9f24e766e0906b7a49753
Youssef <youssefb@truveta.com>
2024-09-14 14:18:33 -0700

    Messed with a lot of settings, it now runs! Example page using MUI is also included

--- cs-frontend/src/createEmotionCache.ts
+++
@@ -1,6 +0,0 @@
-import createCache from '@emotion/cache';
-
-// Create an instance of Emotion's cache to handle SSR for MUI.
-export default function createEmotionCache() {
-  return createCache({ key: 'css', prepend: true });
-}
