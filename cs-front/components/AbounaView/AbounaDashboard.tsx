686a316a0bf5f132af597ecd94f48274c9de4479
Matthew Negasi <negasim@spu.edu>
2024-09-18 15:14:08 -0700

    Updating Dashboard to have components.

--- cs-front/components/AbounaView/AbounaDashboard.tsx
+++ cs-front/components/AbounaView/AbounaDashboard.tsx
@@ -2,13 +2,18 @@
 
 import Link from "next/link";
 import DashboardHeader from "./DashboardHeader";
+import UpcomingAppointments from "@/components/custom/UpcomingAppointments";
+import UpdateAvailability from "@/components/custom/UpdateAvailability";
 
 export default function AbounaDashboard() {
   return (
     <div className="flex flex-col min-h-screen">
       <DashboardHeader />
       <main className="flex-1 p-4 lg:p-6">
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4"></div>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <UpdateAvailability />
+          <UpcomingAppointments />
+        </div>
       </main>
     </div>
   );
