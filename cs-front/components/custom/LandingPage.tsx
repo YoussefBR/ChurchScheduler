150baa05c9be633fc6cab68e370748982cd2df6b
Matthew Negasi <negasim@spu.edu>
2024-09-23 10:16:00 -0700

    Adjusting Landing Page routes.

--- cs-front/components/custom/LandingPage.tsx
+++ cs-front/components/custom/LandingPage.tsx
@@ -41,7 +41,7 @@ export default function LandingPage() {
               </div>
               <div className="space-x-4">
                 <Button
-                  className="bg-background text-black"
+                  className="bg-background text-black hover:bg-slate-500"
                   onClick={() => {
                     window.location.href = "/sign-in";
                   }}
@@ -51,10 +51,10 @@ export default function LandingPage() {
                 <Button
                   className="bg-background text-black hover:bg-slate-500"
                   onClick={() => {
-                    window.location.href = "/sign-up";
+                    window.location.href = "/home";
                   }}
                 >
-                  Sign Up
+                  Book an Appointment
                 </Button>
               </div>
             </div>
