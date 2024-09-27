8fb7b903a13774be96984b3621adee133aca9eb8
Nardin Eshak <nardineshakk@gmail.com>
2024-09-27 15:58:04 -0700

    Fixed

--- CSBackend/obj/Debug/net8.0/CSBackend.AssemblyInfo.cs
+++ CSBackend/obj/Debug/net8.0/CSBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce6ffcc673b1e8d344c16555cf3c7514a3a7f58e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f6407e6b07ab7b6bfbb9601236c03cb1814643e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
